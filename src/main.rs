a3f13be2a7876bb89387f21ebe2fdd45b08e4b89
Mathew Robinson <chasinglogic@gmail.com>
2017-05-16 11:15:15 -0400

    update help output

--- src/main.rs
+++ src/main.rs
@@ -60,10 +60,23 @@ fn find(rgx: Regex, dirs: Vec<PathBuf>, paint: bool) {
 }
 
 fn main() {
-    let matches = App::new("fnd, a simpler way to find files")
-        .version("0.2.0")
+    let matches = App::new("fnd")
+        .version("0.3.0")
         .author("Mathew Robinson <mrobinson@praelatus.io>")
-        .about("Finds files")
+        .about("
+Find files by regex.
+
+Copyright (C) 2016 Mathew Robinson <mrobinson@praelatus.io>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the Apache Version 2.0 License
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+You should have recieved a copy of the license with this software if
+not you can view it here: https://www.apache.org/licenses/LICENSE-2.0")
         .setting(AppSettings::TrailingVarArg)
         .arg(Arg::with_name("color")
              .long("color")
